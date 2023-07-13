427009ee682a2ef468bce5338aac619a3e99f2c2
ShaikHussamali <122262727+ShaikHussamali@users.noreply.github.com>
2023-07-13 23:43:45 +0530

    initial setup started

---
+++ src/components/Home.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './home.css'
+function Home() {
+  return (
+    <div className='main'>
+      <div className='left'>
+       <div className='heading'>
+        <h2>MULTOR</h2>
+       </div>
+        <p>Schedule more virtual appointments, online classes, and video consultations with this high-converting lead capture template.</p>
+        <button>Make it yours</button>
+      </div>
+      <div className='right'>
+        <div className='heading'>
+        <h3>Right side</h3>
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+  Button
+</button>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
